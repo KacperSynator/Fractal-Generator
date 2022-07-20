f7cdddfe3b0b12d61cf977e2c2b1a6cb81d0f9c5
KacperSynator <kacper.synator@gmail.com>
2022-07-20 21:23:14 +0200

    add hello world qt gui

--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,20 @@
+#include <QApplication>
+
 #include "bitmap/bitmap.h"
 #include "fractal_generator.h"
+#include "gui/main_window.h"
+
 
 const auto kMaxIterations{1000};
 const auto kImgWidth{800};
 const auto kImgHeight{600};
 
-int main() {
+int main(int argc, char* argv[]) {
+
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
     FractalGenerator fg = FractalGenerator::Build()
                             .MandelbrotFractal(kMaxIterations)
                             .BitmapImage(kImgWidth, kImgHeight)
@@ -14,6 +23,8 @@ int main() {
     fg.AddZoom( Zoom{kImgWidth / 2, kImgHeight / 2, 4.0 / kImgWidth} );
     fg.AddZoom( Zoom{292, 196, 0.1} );
     fg.AddZoom( Zoom{365, 228, 0.1} );
-    
+
     fg.Generate("test");
+
+    return a.exec();
 }
