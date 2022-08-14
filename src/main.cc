4537f650698e188faf99c3c7bb9e630e2c956f54
KacperSynator <kacper.synator@gmail.com>
2022-08-14 21:01:33 +0200

    clean main

--- src/main.cc
+++ src/main.cc
@@ -9,21 +9,5 @@ int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
     auto gui = Gui::CreateGui();
 
-    // FractalGenerator fg = FractalGenerator::Build(kImgWidth, kImgHeight, kMaxIterations)
-    //                                        .MandelbrotFractal()
-    //                                        .BitmapImage()
-    //                                        .HistColoring();
-
-    // fg.AddColorRange(0.0, 0, 0, 255);
-    // fg.AddColorRange(0.05, 255, 99, 71);
-    // fg.AddColorRange(0.08, 255, 215, 0);
-    // fg.AddColorRange(1.0, 255, 255, 255);
-
-    // fg.AddZoom(Zoom{kZoomStartX, kZoomStartY, kZoomStartSCale});
-    // fg.AddZoom(Zoom{292, 196, 0.1});
-    // fg.AddZoom(Zoom{365, 228, 0.1});
-
-    // fg.Generate("test");
-
     return a.exec();
 }
